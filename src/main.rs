ed3d2d42cc008a02041a0709c8a65a64c08c992f
Wim Bonekamp <wim@wibosa.nl>
2020-03-19 09:57:50 +0100

    Rust by examples additions rusty1 v0.1.2020
    Signed-off-by: Wim Bonekamp <wim@wibosa.nl>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+//mod my_mod;
+extern crate my_mod;
+
 fn main() {
     println!("Hello,2020 rustyapp  IDEA 19.04 Travis world!");
     greet();
-    //let x = std::f64::consts::PI;
+    let _x = std::f64::consts::PI;
     //rust by example
     //https://doc.rust-lang.org/stable/rust-by-example/hello/print.html
     comments();
@@ -267,10 +270,7 @@ fn do_traits() {
     impl Animal for Sheep {
         // `Self` is the implementor type: `Sheep`.
         fn new(name: &'static str) -> Sheep {
-            Sheep {
-                name: name,
-                naked: false,
-            }
+            Sheep { name, naked: false }
         }
 
         fn name(&self) -> &'static str {
@@ -348,4 +348,48 @@ fn do_derive() {
     };
 
     println!("One foot is {} than one meter.", cmp);
+}
+
+fn function() {
+    println!("called `function()`");
+}
+
+fn do_mymod() {
+    // Modules allow disambiguation between items that have the same name.
+
+    function();
+    my_mod::function();
+
+    // Public items, including those inside nested modules, can be
+    // accessed from outside the parent module.
+    my_mod::indirect_access();
+    //my_mod::nested::function();
+    my_mod::call_public_function_in_my_mod();
+
+    // pub(crate) items can be called from anywhere in the same crate
+    my_mod::public_function_in_crate();
+
+    // pub(in path) items can only be called from within the mode specified
+    // Error! function `public_function_in_my_mod` is private
+    //my_mod::nested::public_function_in_my_mod();
+    // TODO ^ Try uncommenting this line
+
+    // Private items of a module cannot be directly accessed, even if
+    // nested in a public module:
+
+    // Error! `private_function` is private
+    //my_mod::private_function();
+    // TODO ^ Try uncommenting this line
+
+    // Error! `private_function` is private
+    //my_mod::nested::private_function();
+    // TODO ^ Try uncommenting this line
+
+    // Error! `private_nested` is a private module
+    //my_mod::private_nested::function();
+    // TODO ^ Try uncommenting this line
+
+    // Error! `private_nested` is a private module
+    //my_mod::private_nested::restricted_function();
+    // TODO ^ Try uncommenting this line
 }
