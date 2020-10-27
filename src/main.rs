2c1357e3b79227892540ba709bdd3d291767b69b
Wim Bonekamp <wim@wibosa.nl>
2020-10-27 11:03:47 +0100

    Rust by examples additions msvc test rusty1 v0.1.2020
    Signed-off-by: Wim Bonekamp <wim@wibosa.nl>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use crate::my_mod;
-
+//use rusty_lib;
 fn main() {
     println!("Hello,2020 rustyapp  IDEA 19.04 Travis world!");
     greet();
@@ -11,6 +10,7 @@ fn main() {
     do_struct();
     do_traits();
     do_derive();
+    //rusty_lib
 }
 
 fn greet() {
@@ -347,48 +347,4 @@ fn do_derive() {
     };
 
     println!("One foot is {} than one meter.", cmp);
-}
-
-fn function() {
-    println!("called `function()`");
-}
-
-fn do_mymod() {
-    // Modules allow disambiguation between items that have the same name.
-
-    function();
-    my_mod::function();
-
-    // Public items, including those inside nested modules, can be
-    // accessed from outside the parent module.
-    my_mod::indirect_access();
-    //my_mod::nested::function();
-    my_mod::call_public_function_in_my_mod();
-
-    // pub(crate) items can be called from anywhere in the same crate
-    my_mod::public_function_in_crate();
-
-    // pub(in path) items can only be called from within the mode specified
-    // Error! function `public_function_in_my_mod` is private
-    //my_mod::nested::public_function_in_my_mod();
-    // TODO ^ Try uncommenting this line
-
-    // Private items of a module cannot be directly accessed, even if
-    // nested in a public module:
-
-    // Error! `private_function` is private
-    //my_mod::private_function();
-    // TODO ^ Try uncommenting this line
-
-    // Error! `private_function` is private
-    //my_mod::nested::private_function();
-    // TODO ^ Try uncommenting this line
-
-    // Error! `private_nested` is a private module
-    //my_mod::private_nested::function();
-    // TODO ^ Try uncommenting this line
-
-    // Error! `private_nested` is a private module
-    //my_mod::private_nested::restricted_function();
-    // TODO ^ Try uncommenting this line
 }
---
+++ src/mod.rs
@@ -0,0 +1,39 @@
+fn do_mymod() {
+    // Modules allow disambiguation between items that have the same name.
+
+    function();
+    my_mod::function();
+
+    // Public items, including those inside nested modules, can be
+    // accessed from outside the parent module.
+    my_mod::indirect_access();
+    //my_mod::nested::function();
+    my_mod::call_public_function_in_my_mod();
+
+    // pub(crate) items can be called from anywhere in the same crate
+    my_mod::public_function_in_crate();
+
+    // pub(in path) items can only be called from within the mode specified
+    // Error! function `public_function_in_my_mod` is private
+    //my_mod::nested::public_function_in_my_mod();
+    // TODO ^ Try uncommenting this line
+
+    // Private items of a module cannot be directly accessed, even if
+    // nested in a public module:
+
+    // Error! `private_function` is private
+    //my_mod::private_function();
+    // TODO ^ Try uncommenting this line
+
+    // Error! `private_function` is private
+    //my_mod::nested::private_function();
+    // TODO ^ Try uncommenting this line
+
+    // Error! `private_nested` is a private module
+    //my_mod::private_nested::function();
+    // TODO ^ Try uncommenting this line
+
+    // Error! `private_nested` is a private module
+    //my_mod::private_nested::restricted_function();
+    // TODO ^ Try uncommenting this line
+}
